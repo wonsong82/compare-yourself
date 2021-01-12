f045846d96fd0b63ae851d8d90d0871a3259dd1e
Won Song <wonsong82@gmail.com>
2021-01-12 11:33:07 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var xhr = new XMLHttpRequest();
-xhr.open('POST', 'https://ypvwytsck0.execute-api.us-east-1.amazonaws.com/dev/compare-yourself');
+xhr.open('GET', 'https://ypvwytsck0.execute-api.us-east-1.amazonaws.com/dev/compare-yourself');
 xhr.onreadystatechange = function(e){
     console.log(e.target.response);
 };
 xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(JSON.stringify({age: 26, height: 71, income: 2100}));
+//xhr.send(JSON.stringify({age: 26, height: 71, income: 2100}));
+xhr.send();
+
+
